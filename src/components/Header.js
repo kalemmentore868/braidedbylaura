537f1e910aecfc7ffc9d353b551680d18eee46eb
kalemmentore868 <kalemmalek123@gmail.com>
2022-11-17 17:21:14 -0400

    finished hero

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,38 +2,56 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
+import Dropdown from "react-bootstrap/Dropdown";
+import Button from "react-bootstrap/Button";
 
 const Header = () => {
   return (
-    <Navbar
-      expand="lg"
-      bg="primary"
-      variant="dark"
-      sticky="top"
-      className="p-3"
-    >
+    <Navbar expand="lg" className="p-3">
       <Container>
-        <Navbar.Brand href="/">Benign</Navbar.Brand>
+        <Navbar.Brand href="/">Braided by Laura</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-          <Nav className="justify-content-end">
-            <Nav.Link className="n-link" href="/">
+          <Nav className="ml-auto">
+            <Nav.Link className="n-link active" href="/">
               Home
             </Nav.Link>
             <Nav.Link className="n-link" href="/about">
               About
             </Nav.Link>
-            <Nav.Link className="n-link" href="/services">
-              Services
-            </Nav.Link>
-            <Nav.Link className="n-link" href="/careers">
-              Careers
-            </Nav.Link>
+            <Dropdown as={Nav.Item}>
+              <Dropdown.Toggle
+                as={Nav.Link}
+                id="navbarDropdown"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Services
+              </Dropdown.Toggle>
+              <Dropdown.Menu
+                className="dropdown-menu-right animate slideIn"
+                aria-labelledby="navbarDropdown"
+              >
+                <Nav.Link href="#">Service 1</Nav.Link>
+                <div class="dropdown-divider"></div>
+                <Nav.Link href="#">Service 2</Nav.Link>
+                <div class="dropdown-divider"></div>
+                <Nav.Link href="#">Service 3</Nav.Link>
+                <div class="dropdown-divider"></div>
+                <Nav.Link href="#">Service 4</Nav.Link>
+              </Dropdown.Menu>
+            </Dropdown>
+
             <Nav.Link className="n-link" href="/contact">
               Contact
             </Nav.Link>
           </Nav>
+          <Button size="sm" variant="dark" className="mx-3" href="#">
+            Book Now
+          </Button>
         </Navbar.Collapse>
       </Container>
     </Navbar>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,24 +1,44 @@
-import React from "react";
-import backgroundImage from "../assets/img/hero-bg.avif";
-import Button from "react-bootstrap/Button";
+import React, { useState } from "react";
+import Carousel from "react-bootstrap/Carousel";
+import herobg from "../assets/img/hero-bg.avif";
+import herobg1 from "../assets/img/hero-bg1.avif";
+import herobg2 from "../assets/img/hero-bg2.avif";
 
 const Hero = () => {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
   return (
-    <section
-      className="text-center bg-image hero-bg"
-      style={{ backgroundImage: `url(${backgroundImage})` }}
-    >
-      <div className="mask d-flex justify-content-center align-items-center h-100">
-        <div className="hero-content">
-          <h2 className="mb-3 text-white">
-            Home care services for the <br /> ones that matter most
-          </h2>
-          <Button variant="primary" className="me-4">
-            Find a Caregiver
-          </Button>
-          {"  "} <Button variant="outline-light">Become a Caregiver</Button>
-        </div>
-      </div>
+    <section className="hero-styles">
+      <Carousel activeIndex={index} onSelect={handleSelect}>
+        <Carousel.Item>
+          <img className="d-block w-100 fix" src={herobg} alt="First slide" />
+          <Carousel.Caption className="caption">
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100 fix" src={herobg1} alt="Second slide" />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100 fix" src={herobg2} alt="Third slide" />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </section>
   );
 };
